9391e176d3c7e3d7d9441cda67dd100c25d02363
ophir88 <ophir.abitbol@mail.huji.ac.il>
2015-04-30 00:02:57 +0300

    final, need to fix blue

---
+++ getLMS.m
@@ -0,0 +1,20 @@
+function [ MA ] = getLMS( option )
+%GETLMS Summary of this function goes here
+%       1 - XYZ scaling
+%       2 - Bradford
+%       3 - Von Kries
+
+if (option == 1)
+MA = eye(3);
+end
+if (option == 2)
+MA = [0.8951 0.2664 -0.1614; -0.7502 1.7135 0.0367  ; 0.0389 -0.0685 1.0296];      
+end
+if(option == 3)
+MA =  [0.4002400 0.7076 -0.0808100; -0.2263 1.16532 0.0457; 0 0 0.91822];
+end
+
+    
+    
+end
+
--- getWhite.m
+++ getWhite.m
@@ -1,48 +1,49 @@
-function [ flashCoeff ] = getWhite( grayCard )
+function [ flashCoeff ] = getWhite( grayCard)
 %GETWHITE Summary of this function goes here
-%   Detailed explanation goes here
+%   grayCard- image of crayCard
+%   XYZ: 0 for RGB, 1 for XYZ
 
 
+%  read image
 img = imread(grayCard);
-[rows cols dim] = size(img);
+% imgD = img;
+%  transform to double
+imgD = im2double(img);
+
+% crop the gray card out of the image (basicaly just looked where the card 
+% starts and ends in the image,
+%  there is nothing sophisticated about those points.. :) ):
+imgD = imgD(1041:2522, 661:1848 , :);
+
+[rows cols dim] = size(imgD);
 n = rows*cols;
 
-imgD = im2double(img);
-syms a b
-
-maxVal = max(max(max(imgD)));
-minVal = min(min(min(imgD)));
-
-eq1 = 1==a*maxVal+b;
-eq2 = 0 ==a*minVal+b;
-sol = solve(eq1,eq2);
-a = double(sol.a);
-b = double(sol.b);
-
-imgD = a*imgD + b;
-imgD = imgD.^(1/2.2);
-%get average of each color:
-
+%find the average gray color (it says RGB, but works just as well for LMS):
 Rav = sum(sum(imgD(:,:,1))')/n;
 Gav = sum(sum(imgD(:,:,2))')/n;
 Bav = sum(sum(imgD(:,:,3))')/n;
-allAv = (Rav + Gav + Bav)/3;
-K = allAv/0.82;
+allAv = (Rav + Gav + Bav)/(3*0.18);
+% K = allAv/0.82;
+% coefficiants to get each channel as close to the average:
 Rcoeff = Rav/allAv;
 Gcoeff = Gav/allAv;
 Bcoeff = Bav/allAv;
-flashCoeff = [Rcoeff , Gcoeff , Bcoeff]*K;
-imgDFix = imgD;
-imgDFix(:,:,1) = imgDFix(:,:,1)/flashCoeff(1);
-imgDFix(:,:,2) = imgDFix(:,:,2)/flashCoeff(2);
-imgDFix(:,:,3) = imgDFix(:,:,3)/flashCoeff(3);
-imgDFix=im2uint8(imgDFix);
-figure;
-subplot(1,2,1);
-imgD=im2uint8(imgD);
-imshow(imgD);
-subplot(1,2,2);
-imshow(imgDFix);
+% L of the flash:
+flashCoeff = [Rcoeff , Gcoeff , Bcoeff];
+
+% % show a color fix:
+% imgDFix = imgD;
+% imgDFix(:,:,1) = imgDFix(:,:,1)/flashCoeff(1);
+% imgDFix(:,:,2) = imgDFix(:,:,2)/flashCoeff(2);
+% imgDFix(:,:,3) = imgDFix(:,:,3)/flashCoeff(3);
+% imgDFix = colorFix(imgDFix);
+% imgDFix=im2uint8(imgDFix);
+% figure;
+% subplot(1,2,1);
+% imgD=im2uint8(imgD);
+% imshow(imgD);
+% subplot(1,2,2);
+% imshow(imgDFix);
 's';
 end
 
